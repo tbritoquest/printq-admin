42a535dbfc9d2fd5be2fb132988b4ea6e5aa009f
Thalia Brito <thaliabrito@MacBook-Pro.home>
2022-03-08 20:56:13 -0500

    Create postcards page

---
+++ src/router/views/demo.vue
@@ -0,0 +1,15 @@
+<script>
+    import Layout from '../layouts/main'
+    import PageHeader from '@/components/page-header'
+
+    export default {
+        components: { Layout, PageHeader },
+    }
+</script>
+
+<template>
+    <Layout>
+        <PageHeader :title="title" :items="items" />
+        This is New Page
+    </Layout>
+</template>
---
+++ src/router/views/products/printing/postcards.vue
@@ -0,0 +1,33 @@
+
+<template>
+    <Layout>
+        <PageHeader :title="title" :items="items" />
+         <h1>Postcards</h1>
+    </Layout>
+   
+</template>
+
+
+<script>
+    import Layout from '../../../layouts/main.vue'
+    import PageHeader from '@/components/page-header';
+
+    export default {
+       
+        components: { Layout,PageHeader},
+        data() {
+            return {
+            title: "Postcards",
+            items: [
+                    {
+                    text: "Skote",
+                    },
+                    {
+                    text: "Postcards",
+                    active: true,
+                    },
+                ]
+            }
+        }
+    }
+</script>
