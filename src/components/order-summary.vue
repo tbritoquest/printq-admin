9455d39ac39981b6c27f3878e5cde9bc030478a5
Thalia Brito <thaliabrito@MacBook-Pro.home>
2022-04-19 21:14:49 -0400

    Add padding to button

--- src/components/order-summary.vue
+++ src/components/order-summary.vue
@@ -50,7 +50,7 @@
 
         </div>
 
-        <div style="padding:1em;text-align:center;">
+        <div style="padding:1em;padding-bottom:2em;text-align:center;">
           <button type="button" class="btn btn-primary rounded-pill" v-if="cart && cart.length>0" @click="submitOrder()" >Place Order</button>
         </div>
 
